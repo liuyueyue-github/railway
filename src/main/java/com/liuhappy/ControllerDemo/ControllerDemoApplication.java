9c294f7b0f5d6cdb9516c9fadd27475ff2bf1f2d
yueyue.liu <yueyue.liu@sy-fintech.com>
2022-06-27 14:38:35 +0800

    定时任务demo

--- src/main/java/com/liuhappy/ControllerDemo/ControllerDemoApplication.java
+++ src/main/java/com/liuhappy/ControllerDemo/ControllerDemoApplication.java
@@ -2,11 +2,13 @@ package com.liuhappy.ControllerDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Lenovo
  */
 @SpringBootApplication
+@EnableScheduling
 public class ControllerDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/liuhappy/ControllerDemo/schedule/TaskService.java
@@ -0,0 +1,22 @@
+package com.liuhappy.ControllerDemo.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Grin
+ * @Date 2022/6/27
+ * @Description
+ */
+@Component
+public class TaskService {
+    @Scheduled(cron = "0 0/1 * * * ? ")
+    public void task() {
+        System.out.println("Thread Name : "
+                + Thread.currentThread().getName() + "  i am a task : date ->  "
+                + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
